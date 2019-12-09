a72b60449449fecc93298720fce2e87a16f6a413
Klaudia <kklaudiaw@gmail.com>
2019-12-09 16:12:56 +0100

    added eslint, prettier and changed js functions names

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,14 @@
 const panels = document.querySelectorAll('div.panel');
-const text = document.querySelectorAll('p.panel__text')
 
-function managePanel(){
-    this.classList.toggle('panel--open');
+function togglePanel() {
+  this.classList.toggle('panel--open');
 }
 
-function manageText(e){
-    console.log(e.propertyName);
-    if (e.propertyName.includes('flex')){
-       this.classList.toggle('panel--active'); 
-    }
+function toggleText(e) {
+  if (e.propertyName.includes('flex')) {
+    this.classList.toggle('panel--active');
+  }
 }
 
-panels.forEach(panel=>panel.addEventListener('click', managePanel));
-panels.forEach(text=>text.addEventListener('transitionend', manageText))
+panels.forEach((panel) => panel.addEventListener('click', togglePanel));
+panels.forEach((panel) => panel.addEventListener('transitionend', toggleText));
