78923bfced0cc0e127978fc95579b77b326c8191
Klaudia <kklaudiaw@gmail.com>
2019-12-09 17:51:14 +0100

    prettier, closure

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,16 @@
-const panels = document.querySelectorAll('div.panel');
+{
+  const panels = document.querySelectorAll('div.panel');
 
-function togglePanel() {
-  this.classList.toggle('panel--open');
-}
-
-function toggleText(e) {
-  if (e.propertyName.includes('flex')) {
-    this.classList.toggle('panel--active');
+  function togglePanel() {
+    this.classList.toggle('panel--open');
   }
-}
 
-panels.forEach((panel) => panel.addEventListener('click', togglePanel));
-panels.forEach((panel) => panel.addEventListener('transitionend', toggleText));
+  function toggleText(e) {
+    if (e.propertyName.includes('flex')) {
+      this.classList.toggle('panel--active');
+    }
+  }
+
+  panels.forEach((panel) => panel.addEventListener('click', togglePanel));
+  panels.forEach((panel) => panel.addEventListener('transitionend', toggleText));
+}
